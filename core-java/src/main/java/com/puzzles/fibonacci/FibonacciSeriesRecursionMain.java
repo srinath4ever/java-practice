615f877d28681debbbb6b75a64aca745d77667f3
Srinath Rayabarapu <naath.r@gmail.com>
2020-12-18 08:20:38 +0530

    Dynamic programming practice

--- core-java/src/main/java/com/puzzles/fibonacci/FibonacciSeriesRecursionMain.java
+++ core-java/src/main/java/com/puzzles/fibonacci/FibonacciSeriesRecursionMain.java
@@ -1,23 +1,28 @@
 package com.puzzles.fibonacci;
 
 /**
- * given a number, program prints the fibonacci series of length. Uses recursion!
- * 
- * @author Srinath.Rayabarapu
+ * given a number, print fibinocci series till the number index, using recursion.
  *
+ * Fibinocci series: 1 1 2 3 5 8 13 21 34 55
+ *
+ * TC: O(2^n)
+ * SC: O(n)
+ * https://www.youtube.com/watch?v=oBt53YbR9Kk
+ *
+ * @author Srinath.Rayabarapu
  */
 public class FibonacciSeriesRecursionMain {
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++)
+		for (int i = 1; i <= 10; i++)
 			System.out.println(findFibinocciNumber(i));
 	}
 	
 	public static int findFibinocciNumber(int index) {
-		if(index == 0 || index == 1 ){
-			return index;
+		if(index <= 2){
+			return 1;
 		}
-		return (findFibinocciNumber(index-1)+ findFibinocciNumber(index-2));
+		return (findFibinocciNumber(index-1) + findFibinocciNumber(index-2));
 	}
 	
 }
