eeb2b1b3100f3d86be891fb73a1595e4296d3a47
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2021-01-31 18:03:12 +0530

    best solution from Kata

--- core-java/src/main/java/com/core/codewars/ShortestWord.java
+++ core-java/src/main/java/com/core/codewars/ShortestWord.java
@@ -1,5 +1,7 @@
 package com.core.codewars;
 
+import java.util.stream.Stream;
+
 public class ShortestWord {
 
     public static int findShort(String s) {
@@ -8,6 +10,14 @@ public class ShortestWord {
             min = Math.min(min, word.length());
         }
         return min;
+    }
+
+    // best solution from Kata
+    public static int bestFindShort(String s) {
+        return Stream.of(s.split(" "))
+                .mapToInt(String::length)
+                .min()
+                .getAsInt();
     }
 
 }
